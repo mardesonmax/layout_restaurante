cbc9023cc6a9227fd8d1137f15cf6eb226a8b23a
mardesonmax <mardesonpereira2018@gmail.com>
2020-09-21 17:36:04 -0300

    Adicionando arquivos

---
+++ README.md
@@ -0,0 +1,6 @@
+# Layout para restaurantes
+### Clique <a href="https://mardesonmax.github.io/layout_restaurante/">aqui</a> para visualizar a página
+
+<a href="https://mardesonmax.github.io/layout_restaurante/">
+<img src="assets/image/layout/layout.png">
+</a>
---
+++ assets/js/script.js
@@ -0,0 +1,205 @@
+$(document).ready(_ => {
+    $(".loader").hide(); 
+    /*===================================================
+        OLW
+    ===================================================*/
+    $('.owl-carousel').owlCarousel({  
+        loop:false,
+        margin:10,
+        nav:false,
+        smartSpeed: 250,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+
+    /*===================================================
+       Menu mobile 
+    ===================================================*/
+    function mudarIconeMenu() {
+        let menu = $('.navbar-toggler'),
+            abrir = 'fa-bars',
+            exit = 'fa-times'
+        if($(menu).hasClass(abrir)) {
+            menu.removeClass(abrir).addClass(exit)
+        } else {
+            menu.removeClass(exit).addClass(abrir)
+        }
+    }
+
+    
+    $('.navbar-toggler').on('click', e => {
+        mudarIconeMenu()
+    })
+
+    /*===================================================
+       Vizualizar imagem destaque
+    ===================================================*/
+    $('#destaques .btn').on('click', e => {
+        e.preventDefault()
+
+        let url = $(e.target).closest('.item').find('img').attr('src')
+        $('#visualizar-imagem img').attr('src', url)
+
+        $('#visualizar-imagem').addClass('ativo')
+        setTimeout(() => {
+            $('#visualizar-imagem .imagem').addClass('foto-start')   
+        }, 100);
+
+        $('html, body').css('overflow', 'hidden')
+
+    })
+
+    function fecharImagem(e) {
+        e.preventDefault()
+        
+        $('#visualizar-imagem .imagem').removeClass('foto-start') 
+        setTimeout(() => {
+            $('#visualizar-imagem').removeClass('ativo')   
+        }, 300);
+        
+        $('html, body').css('overflow', 'auto')
+    }
+
+    $('#visualizar-imagem .btn').on('click', e => {
+        fecharImagem(e)
+    })
+    $('#visualizar-imagem .fundo-preto').on('click', e => {
+        fecharImagem(e)
+    })
+
+     /*===================================================
+        Menu animado
+    ===================================================*/
+    const topoMenu = $('#topo-menu .nav-link'),
+          classMenuAtivo = ('ativo'),
+          footerMenu = $('.menu-footer a')
+
+    function selecionarMenu(menu) {
+        let id = $(menu).attr('href'),
+            menuH = $('#topo-menu').height(),
+            alturaTop = $(id).offset().top - menuH
+        
+        $('html, body').animate({
+            scrollTop: alturaTop
+        }, 300)
+    }
+    function ativarMenu (menu) {
+        topoMenu.removeClass(classMenuAtivo)          
+        
+        let idMenu = `#${menu}`
+
+        topoMenu.each((e, value) => {
+            let allId = $(value).attr('href')
+
+            if(idMenu === allId) {
+                $(value).addClass(classMenuAtivo)
+            }
+        })       
+    }
+    footerMenu.on('click', e => {
+        e.preventDefault()
+
+        selecionarMenu(e.target)
+    })
+
+    topoMenu.on('click', (e) => {
+        e.preventDefault()
+
+        selecionarMenu(e.target)
+        
+    })
+
+    /*===================================================
+        Anime na pagina
+    ===================================================*/
+    const target = $('.anime'),
+          animeClass = 'anime-start'
+          
+    function animeScroll() {
+        let documentTop = $(document).scrollTop(),
+            offset = $(window).height()
+
+        target.each(function() {
+            let itemTop = $(this).offset().top
+           
+            if(documentTop > itemTop - offset * 3/4) {
+                $(this).addClass(animeClass)
+            } else {
+                $(this).removeClass(animeClass)
+            }
+
+            //Ativando menu 
+            if(documentTop > itemTop - offset * 1.5/4) {
+                let idMenu = $(this).attr('id')
+
+                ativarMenu(idMenu)
+            } 
+        })
+
+        
+
+        // Fixando menu 
+        let menu = $('#topo-menu'),
+            topoH = $('#topo').height(),
+            menuH = menu.height()
+        
+        if(documentTop >= topoH ) {
+            menu.addClass('menu-fixo')
+        } else if(documentTop < topoH - menuH){
+            menu.removeClass('menu-fixo')
+        }
+    }
+
+    animeScroll()
+    $(window).resize(_ => {
+        animeScroll()
+    })
+    $(document).scroll(_ => {
+        animeScroll()
+    })
+
+    /*===================================================
+        Cardápio
+    ===================================================*/
+    const cardapio = $('.cardapio-container'),
+          menuItem = $('.menu-item'),
+          menuClass = 'menu-ativo',
+          cardapioContainer = $('.cardapio-container')
+
+    function cardapioClick (e) {        
+        let menu = $(e),
+            id = $(e).attr('href')
+        menuItem.removeClass(menuClass)
+
+        menu.addClass(menuClass)
+
+        cardapioContainer.slideUp(300)
+
+        setTimeout(_ => {
+            $(id).slideDown(300)
+        }, 300)
+    }
+
+    menuItem.on('click', (e) => {   
+        e.preventDefault()   
+        cardapioClick(e.target)        
+    })
+
+    menuItem.each((e, value) => {
+        if(e === 0) {
+            cardapioClick(value)
+        }
+    })
+
+   
+
+})
